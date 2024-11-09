8d0c500dc9e93c6349f6ca2d567068b0ffbfc17d
hzswdef <ostelia.group@gmail.com>
2024-11-09 08:40:38 +0200

    Trello #Z3K8Yd1J: FE: Added useTitle() hook to change the browser tab title on the "Home" and "Post" pages. FE: Fixed typo in "app/src/components/ckeditor/CKEditor.tsx" file.

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,4 +1,13 @@
+import useTitle from "@hooks/useTitle";
+import { useEffect } from "react";
+
 const Home = () => {
+  const updateTitle = useTitle();
+
+  useEffect(() => {
+    updateTitle("Home");
+  }, [updateTitle]);
+
   return <div>Home</div>;
 };
 
