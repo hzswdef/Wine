88554e526053a8a0eb85e42230b5da7ac03cfd17
hzswdef <ostelia.group@gmail.com>
2024-11-21 06:29:35 +0200

    Trello #Zyv6GJNy: FE: Applied "react-router-dom" future flags.

--- app/src/routes/Router.tsx
+++ app/src/routes/Router.tsx
@@ -50,9 +50,24 @@ const Router = () => {
 		}
 	];
 
-	const router = createBrowserRouter(routes);
+	const router = createBrowserRouter(routes, {
+		future: {
+			v7_relativeSplatPath: true,
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_partialHydration: true,
+			v7_skipActionErrorRevalidation: true
+		}
+	});
 
-	return <RouterProvider router={router} />;
+	return (
+		<RouterProvider
+			router={router}
+			future={{
+				v7_startTransition: true
+			}}
+		/>
+	);
 };
 
 export default Router;
